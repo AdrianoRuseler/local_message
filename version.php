c83dad9cc46e046f093d841bd192a09820d54295
Adriano Ruseler <ruseler@inep.ufsc.br>
2022-01-14 11:04:32 -0300

    Create version.php

---
+++ version.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   local_message
+ * @copyright 2022, Adriano Ruseler <adrianoruseler@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2022011400;
+$plugin->requires = 2020061500; // https://docs.moodle.org/dev/Releases.
+$plugin->component = 'local_message';
+$plugin->maturity = MATURITY_ALPHA; // Supported value is any of the predefined constants MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE. 
+$plugin->release = 'v0.0.1';
